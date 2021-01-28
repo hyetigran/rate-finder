9a94716ddab874562f1d047b27f9863a1330b786
Tigran Asriyan <tigran.asriyan@gmail.com>
2021-01-27 23:24:27 -0500

    expo app boilerplate with map

--- types.tsx
+++ types.tsx
@@ -4,12 +4,12 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
+  Search: undefined;
   TabTwo: undefined;
 };
 
 export type TabOneParamList = {
-  TabOneScreen: undefined;
+  MapRateScreen: undefined;
 };
 
 export type TabTwoParamList = {
